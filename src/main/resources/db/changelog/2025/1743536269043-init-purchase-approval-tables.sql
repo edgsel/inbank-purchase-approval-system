01667e6a3ad56632c2c208fef269f21cc0509918
edgsel <edgar.selihov@gmail.com>
2025-04-02 20:23:11 +0300

    IN-00 - improve migration script

--- src/main/resources/db/changelog/2025/1743536269043-init-purchase-approval-tables.sql
+++ src/main/resources/db/changelog/2025/1743536269043-init-purchase-approval-tables.sql
@@ -1,19 +1,22 @@
 -- liquibase formatted sql logicalFilePath:classpath:/db/changelog/2025/1743536269043-init-purchase-approval-tables.sql 
--- changeset author:edgsel
+-- changeset edgsel:IN-00-init-purchase-approval-tables
+
+CREATE TYPE customer_status AS ENUM ('ACTIVE', 'INACTIVE');
+CREATE TYPE purchase_status AS ENUM ('APPROVED', 'REJECTED', 'APPROVED_PARTIALLY');
 
 CREATE TABLE profile (
     id SERIAL PRIMARY KEY,
     financial_capacity_factor INTEGER NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    modified_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE TABLE customer (
     id SERIAL PRIMARY KEY,
     profile_id INTEGER NOT NULL,
-    status VARCHAR(10) NOT NULL,
+    status customer_status NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
     CONSTRAINT fk_customer_profile FOREIGN KEY (profile_id) REFERENCES profile(id) ON DELETE CASCADE
 );
 
@@ -22,8 +25,11 @@ CREATE TABLE purchase (
     customer_id INTEGER NOT NULL,
     amount DECIMAL(18,2) NOT NULL,
     max_allowed_amount DECIMAL(18,2) NOT NULL,
-    status VARCHAR(20) NOT NULL,
+    status purchase_status NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    modified_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
     CONSTRAINT fk_purchase_customer FOREIGN KEY (customer_id) REFERENCES customer(id) ON DELETE CASCADE
 );
+
+CREATE INDEX idx_customer_profile_id ON customer(profile_id);
+CREATE INDEX idx_purchase_customer_id ON purchase(customer_id);
